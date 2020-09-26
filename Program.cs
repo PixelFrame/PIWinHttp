84975095cd8de5ab03ddf85538d0c46b37b50078
Pixel Frame <pm421@live.com>
2020-09-26 10:27:47 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PIWinHttp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntPtr hSession = WinHttp.WinHttpOpen("PINVOKE WINHTTP CLIENT/1.0", AccessType.WINHTTP_ACCESS_TYPE_NO_PROXY, "", "", 0);
+
+            WINHTTP_AUTOPROXY_OPTIONS winHttpAutoProxyOptions = new WINHTTP_AUTOPROXY_OPTIONS();
+            WINHTTP_PROXY_INFO winHttpProxyInfo = new WINHTTP_PROXY_INFO();
+
+            winHttpAutoProxyOptions.dwFlags = AutoProxyFlag.WINHTTP_AUTOPROXY_AUTO_DETECT;
+            winHttpAutoProxyOptions.dwAutoDetectFlags = AutoDetectFlag.WINHTTP_AUTO_DETECT_TYPE_DHCP | AutoDetectFlag.WINHTTP_AUTO_DETECT_TYPE_DNS_A;
+            winHttpAutoProxyOptions.fAutoLogonIfChallenged = true;
+
+            Console.WriteLine(WinHttp.WinHttpGetProxyForUrl(hSession, "http://example.com", ref winHttpAutoProxyOptions, ref winHttpProxyInfo));
+
+            Console.WriteLine(winHttpProxyInfo.lpszProxy);
+
+            Console.WriteLine(WinHttp.WinHttpCloseHandle(hSession));
+
+            winHttpProxyInfo = new WINHTTP_PROXY_INFO();
+            winHttpProxyInfo.dwAccessType = AccessType.WINHTTP_ACCESS_TYPE_DEFAULT_PROXY;
+            Console.WriteLine(WinHttp.WinHttpGetDefaultProxyConfiguration(ref winHttpProxyInfo));
+
+            Console.ReadLine();
+        }
+    }
+}
